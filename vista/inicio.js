6b3df28f5c68445ba57bc2b132c4ad4800a2be4e
Mario <mario.albornozc@utem.cl>
2019-01-28 16:19:41 -0300

    actualizar vista de ramos usuario

--- vista/inicio.js
+++ vista/inicio.js
@@ -16,3 +16,28 @@ $('.form').hide();
 function despliegueForm(){
     $('.form').slideToggle();
 }
+
+$('.Ramos').hide();
+function despliegueRamos(){
+    $('.Ramos').slideToggle();
+}
+$('.Ramos2').hide();
+function despliegueRamos2(){
+    $('.Ramos2').slideToggle();
+}
+$('.Ramos3').hide();
+function despliegueRamos3(){
+    $('.Ramos3').slideToggle();
+}
+$('.Ramos4').hide();
+function despliegueRamos4(){
+    $('.Ramos4').slideToggle();
+}
+$('.Ramos5').hide();
+function despliegueRamos5(){
+    $('.Ramos5').slideToggle();
+}
+$('.Ramos6').hide();
+function despliegueRamos6(){
+    $('.Ramos6').slideToggle();
+}
