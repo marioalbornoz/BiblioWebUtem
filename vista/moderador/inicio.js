15b11c42f24c5ae79084ebf429510d7f5cedf0b4
Mario <mario.albornozc@utem.cl>
2019-01-27 23:30:14 -0300

    agregando boton

--- vista/moderador/inicio.js
+++ vista/moderador/inicio.js
@@ -1,11 +1,15 @@
 
 $('.Asignaturas21030').hide();
 $('.Asignaturas21040').hide();
+$('.Asignaturas21075').hide();
 function despliegue21030(){
     $('.Asignaturas21030').slideToggle();
 }
 function despliegue21040(){
     $('.Asignaturas21040').slideToggle();
+}
+function despliegue21075(){
+    $('.Asignaturas21075').slideToggle();
 }
 
    
